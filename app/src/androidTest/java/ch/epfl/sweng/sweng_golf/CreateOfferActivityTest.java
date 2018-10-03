398af8052c2b4f8f2b0bb78cf16ce97954bbedbc
Sébastien Fulpius <sebastien.fulpius@epfl.ch>
2018-10-03 22:35:50 +0200

    Code cleanup

--- app/src/androidTest/java/ch/epfl/sweng/sweng_golf/CreateOfferActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/sweng_golf/CreateOfferActivityTest.java
@@ -1,7 +1,5 @@
 package ch.epfl.sweng.sweng_golf;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -12,13 +10,9 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
