c33ecde20eb17e013bfb7ce03fae75bf56f53398
Sébastien Fulpius <sebastien.fulpius@epfl.ch>
2018-10-03 22:02:53 +0200

    Add tests for CreateOfferActivity

---
+++ app/src/androidTest/java/ch/epfl/sweng/sweng_golf/CreateOfferActivityTest.java
@@ -0,0 +1,41 @@
+package ch.epfl.sweng.sweng_golf;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class CreateOfferActivityTest {
+
+    @Rule
+    public final ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+    @Test
+    public void errorMessageDisplayed() {
+        onView(withId(R.id.create_offer_button)).perform(click());
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.error_message)).check(matches(isDisplayed()));
+
+    }
+}
