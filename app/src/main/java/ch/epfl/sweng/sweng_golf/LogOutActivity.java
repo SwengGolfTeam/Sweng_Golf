1d762a7e60d861031da6f33f9248f6dc4ba5c022
iMac <iMac@iMac-de-Mac.local>
2018-10-04 18:18:56 +0200

    Fix Travis Problem With Graddle
    Adding implementation of the latest versions of support

--- app/src/main/java/ch/epfl/sweng/sweng_golf/LogOutActivity.java
+++ app/src/main/java/ch/epfl/sweng/sweng_golf/LogOutActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -52,6 +53,7 @@ public class LogOutActivity extends AppCompatActivity {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 if(firebaseAuth.getCurrentUser() == null){
+                    Toast.makeText(LogOutActivity.this, "Goodbye", Toast.LENGTH_SHORT).show();
                     startActivity(new Intent(LogOutActivity.this, SignInActivity.class));
                 }
             }
