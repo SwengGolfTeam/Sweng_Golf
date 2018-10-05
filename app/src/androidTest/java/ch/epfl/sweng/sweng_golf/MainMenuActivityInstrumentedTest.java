56d7c02002d42b5d87d6ed9104dde5a89dc29a0b
udamiano <ugo.damiano@epfl.ch>
2018-10-05 11:48:04 +0200

    Remove the Google dependency

--- app/src/androidTest/java/ch/epfl/sweng/sweng_golf/MainMenuActivityInstrumentedTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/sweng_golf/MainMenuActivityInstrumentedTest.java
@@ -5,13 +5,9 @@ import android.support.test.runner.AndroidJUnit4;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.lang.reflect.Field;
 
 import static android.support.test.espresso.contrib.DrawerActions.openDrawer;
 import static org.junit.Assert.assertTrue;
