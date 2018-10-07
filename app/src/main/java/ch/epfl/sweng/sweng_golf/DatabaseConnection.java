7723682544993511e77f64064793a9a395fac1e9
dingepfl <markus.ding@epfl.ch>
2018-10-07 23:32:47 +0200

    Implements Firebase adds the class DatabaseConnection.java that handles everything for the writes and reads to/from the database

---
+++ app/src/main/java/ch/epfl/sweng/sweng_golf/DatabaseConnection.java
@@ -0,0 +1,63 @@
+package ch.epfl.sweng.sweng_golf;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class DatabaseConnection {
+    private final FirebaseDatabase db;
+    private DatabaseReference ref;
+
+    public DatabaseConnection(){
+        db = FirebaseDatabase.getInstance("https://sweng-golf.firebaseio.com/");
+        ref = db.getReference();
+    }
+
+    /**
+     * Write a new offer in the database
+     * @param offerID an ID unique to this offer
+     * @param newOffer the new Offer that needs to be added
+     *
+     */
+    public void writeOffer(String offerID, Offer newOffer){
+        ref = db.getReference();
+        ref.child("offers").child(offerID).setValue(newOffer);
+    }
+
+    /**
+     * Reads all the offers that are in the database
+     *
+     */
+    public void readOffers(){
+        //TODO: see https://www.captechconsulting.com/blogs/firebase-realtime-database-android-tutorial
+    }
+
+    /**
+     * Reads a specific offer from the database
+     * @param offerID the ID of the offer
+     *
+     */
+    public void readOffer(final String offerID){
+        ref = db.getReference("/offers/"+offerID);
+
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Offer offer = dataSnapshot.getValue(Offer.class);
+                Log.d("DB_READ", "offer read: "+offer.getName());
+
+                //TODO: interface.displayOffer(offer)
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("DB_READ", "failed offerID="+offerID);
+            }
+        });
+    }
+}
