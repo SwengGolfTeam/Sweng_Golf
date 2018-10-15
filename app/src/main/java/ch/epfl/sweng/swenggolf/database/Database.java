d7629b21eb0c476b652bd35cf0bb35208cb3a5fa
Sébastien Fulpius <sebastien.fulpius@epfl.ch>
2018-10-15 16:56:45 +0200

    Implement the Database interface

--- app/src/main/java/ch/epfl/sweng/swenggolf/database/Database.java
+++ app/src/main/java/ch/epfl/sweng/swenggolf/database/Database.java
@@ -1,9 +1,32 @@
 package ch.epfl.sweng.swenggolf.database;
 
 
+import android.support.annotation.NonNull;
+
 import java.util.List;
 
+import ch.epfl.sweng.swenggolf.offer.Offer;
+
 public abstract class Database {
+
+    private static Database database = null;
+
+    /**
+     * Use the given database as the return of the getInstance() method.
+     * @param d the fake database
+     */
+    public static void setDebugDatabase(Database d){
+        database = d;
+    }
+
+    public static Database getInstance(){
+        if(database == null){
+            database = new FireDatabase();
+        }
+        return database;
+    }
+
+    protected Database() {}
 
     /**
      * Write an object into the database.
@@ -11,7 +34,7 @@ public abstract class Database {
      * @param id the id of the object
      * @param object the object to write
      */
-    abstract void write(String path, String id, Object object);
+    public abstract void write(@NonNull String path, @NonNull String id, @NonNull Object object);
 
     /**
      * Write an object into the database. Use the listener to check if the write could be executed.
@@ -21,7 +44,8 @@ public abstract class Database {
      * @param listener the onComplete method of the listener will be called with true if it was a
      *                 success or false otherwise.
      */
-    abstract void write(String path, String id, Object object, CompletionListener listener);
+    public abstract void write(@NonNull String path, @NonNull String id, @NonNull Object object,
+                               @NonNull CompletionListener listener);
 
     /**
      * Read a single value in a given path with a given id. It return the value using a listener.
@@ -31,7 +55,8 @@ public abstract class Database {
      *                 onCancelled method will be called
      * @param c the class of the value
      */
-    abstract <T> void read(String path, String id, ValueListener<T> listener, Class<T> c);
+    public abstract <T> void read(@NonNull String path, @NonNull String id,
+                                  @NonNull ValueListener<T> listener, @NonNull Class<T> c);
 
     /**
      * Read a list of value in a given path. It return the value using a listener.
@@ -40,6 +65,17 @@ public abstract class Database {
      *                 onCancelled method will be called
      * @param c the class of the value
      */
-    abstract <T> void readList(String path, ValueListener<List<T>> listener, Class<T> c);
+    public abstract <T> void readList(@NonNull String path,
+                                      @NonNull ValueListener<List<T>> listener,
+                                      @NonNull Class<T> c);
 
+    /**
+     * Read the list of all offers. It return the list using a listener.
+     * @param listener the onDataChange method will be called if we find the value. Otherwise, the
+     *                 onCancelled method will be called
+     */
+
+    public void readOffers(@NonNull ValueListener<List<Offer>> listener){
+        readList("/offers", listener, Offer.class);
+    }
 }
