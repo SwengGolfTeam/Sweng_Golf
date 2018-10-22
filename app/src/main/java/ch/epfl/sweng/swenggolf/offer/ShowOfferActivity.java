f9be3fb8606fd428d155cdf53b62bbdec90b9fb4
ejolles <jolleseric@gmail.com>
2018-10-22 21:29:30 +0200

    Add A Button To Delete An Offer

--- app/src/main/java/ch/epfl/sweng/swenggolf/offer/ShowOfferActivity.java
+++ app/src/main/java/ch/epfl/sweng/swenggolf/offer/ShowOfferActivity.java
@@ -1,17 +1,24 @@
 package ch.epfl.sweng.swenggolf.offer;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
 import ch.epfl.sweng.swenggolf.Config;
 import ch.epfl.sweng.swenggolf.R;
+import ch.epfl.sweng.swenggolf.database.CompletionListener;
+import ch.epfl.sweng.swenggolf.database.Database;
+import ch.epfl.sweng.swenggolf.database.DbError;
 import ch.epfl.sweng.swenggolf.tools.ViewUserFiller;
 
 
@@ -23,7 +30,6 @@ public class ShowOfferActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_offer);
-
         offer = getIntent().getParcelableExtra("offer");
         if (!Config.getUser().getUserId().equals(offer.getUserId())) {
             ImageView button = findViewById(R.id.button_modify_offer);
@@ -61,4 +67,58 @@ public class ShowOfferActivity extends AppCompatActivity {
         intent.putExtra("offer", offer);
         startActivity(intent);
     }
+
+    /**
+     * Launches the DeleteOfferActivity using the current offer.
+     *
+     * @param view the view
+     */
+    public void deleteOffer(View view) {
+        showDeleteAlertDialog();
+    }
+
+    /**
+     * Display the Alert Dialog for the delete.
+     */
+    public void showDeleteAlertDialog(){
+        AlertDialog.Builder builder;
+        builder = new AlertDialog.Builder(this);
+        builder.setTitle("Delete entry")
+                .setMessage("Are you sure you want to delete this entry?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        deleteOfferInDatabase();
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
+    /**
+     * Delete the offer in the database.
+     */
+    private void deleteOfferInDatabase(){
+        Database database = Database.getInstance();
+        CompletionListener listener = new CompletionListener() {
+            @Override
+            public void onComplete(@Nullable DbError databaseError) {
+                if (databaseError == DbError.NONE) {
+                    Toast.makeText(ShowOfferActivity.this, "Offer deleted",
+                            Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(ShowOfferActivity.this, ListOfferActivity.class));
+                } else {
+                    Toast.makeText(ShowOfferActivity.this, "Offer couldn't be deleted",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        };
+        database.remove("/offers", offer.getUuid(), listener);
+    }
+
 }
