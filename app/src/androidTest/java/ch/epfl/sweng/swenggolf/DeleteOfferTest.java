d6a230b98510ba7eb638eaa6659861bd8c99bfbd
Eric <eric.jolles@epfl.ch>
2018-10-23 07:52:55 +0200

    Try To Fix Travis Issues

--- app/src/androidTest/java/ch/epfl/sweng/swenggolf/DeleteOfferTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/swenggolf/DeleteOfferTest.java
@@ -109,18 +109,6 @@ public class DeleteOfferTest {
         onView(withText("Delete entry")).check(matches(isDisplayed()));
     }
 
-    @Test
-    public void toastPositiveAppears() {
-        openListActivity();
-        onView(withId(R.id.offers_recycler_view))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
-        onView(withId(R.id.button_delete_offer)).perform(click());
-        onView(withText(android.R.string.yes)).perform(click());
-        onView(withText(R.string.offer_deleted))
-                .inRoot(withDecorView(CoreMatchers.not(is(
-                        mActivityRule.getActivity().getWindow().getDecorView()))))
-                .check(matches(isDisplayed()));
-    }
 
     @Test
     public void cancelDialog() {
@@ -142,6 +130,10 @@ public class DeleteOfferTest {
         onView(withId(R.id.show_offer_title)).check(matches(withText(TITLE1)));
         onView(withId(R.id.button_delete_offer)).perform(click());
         onView(withText(android.R.string.yes)).perform(click());
+        onView(withText(R.string.offer_deleted))
+                .inRoot(withDecorView(CoreMatchers.not(is(
+                        mActivityRule.getActivity().getWindow().getDecorView()))))
+                .check(matches(isDisplayed()));
         ValueListener vl = new ValueListener() {
             @Override
             public void onDataChange(Object value) {
