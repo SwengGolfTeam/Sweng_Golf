98e4ddb9906d4fd57e16080a4a30cae1d5fa9a84
udamiano <ugo.damiano@epfl.ch>
2018-10-30 20:40:41 +0100

    Fix two identical name for same xml element
    Replace the id show_offer_title from offers_list_row to avoid ambiguity with the title of activity_show_offer.

--- app/src/androidTest/java/ch/epfl/sweng/swenggolf/DeleteOfferTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/swenggolf/DeleteOfferTest.java
@@ -24,10 +24,8 @@ import ch.epfl.sweng.swenggolf.offer.Offer;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 
-import static android.support.test.espresso.matcher.PreferenceMatchers.withTitle;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
