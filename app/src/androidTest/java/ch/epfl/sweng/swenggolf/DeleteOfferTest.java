6b4c3c84426c3f2fe20ccf53df8db8efff80219b
udamiano <ugo.damiano@epfl.ch>
2018-10-30 16:36:49 +0100

    Fix test issues
    Open options menu if button are hidden.

--- app/src/androidTest/java/ch/epfl/sweng/swenggolf/DeleteOfferTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/swenggolf/DeleteOfferTest.java
@@ -1,6 +1,8 @@
 package ch.epfl.sweng.swenggolf;
 
 import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.NoMatchingViewException;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -20,10 +22,14 @@ import ch.epfl.sweng.swenggolf.main.MainMenuActivity;
 import ch.epfl.sweng.swenggolf.offer.Offer;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 
+import static android.support.test.espresso.matcher.PreferenceMatchers.withTitle;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.fail;
@@ -78,11 +84,16 @@ public class DeleteOfferTest {
     }
 
     @Test
-    public void deleteElementTest() throws InterruptedException {
+    public void deleteElementTest() {
         onView(withId(R.id.offers_recycler_view))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
         onView(withId(R.id.show_offer_title)).check(matches(withText(TITLE1)));
-        onView(withId(R.id.button_delete_offer)).perform(click());
+        try {
+            onView(withId(R.id.button_delete_offer)).perform(click());
+        } catch (NoMatchingViewException e) {
+            openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+            onView(withContentDescription(R.string.delete_offer)).perform(click());
+        }
         onView(withText(android.R.string.yes)).perform(click());
         ValueListener vl = new ValueListener() {
             @Override
