1c7088e2a20dfc5d41d1fc754df66bf11e865fbf
Sébastien Fulpius <sebastien.fulpius@epfl.ch>
2018-11-12 15:12:49 +0100

    Change the icon of "My offers"

--- app/src/main/java/ch/epfl/sweng/swenggolf/offer/ListOwnOfferActivity.java
+++ app/src/main/java/ch/epfl/sweng/swenggolf/offer/ListOwnOfferActivity.java
@@ -1,10 +1,15 @@
 package ch.epfl.sweng.swenggolf.offer;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import java.util.List;
 
 import ch.epfl.sweng.swenggolf.Config;
+import ch.epfl.sweng.swenggolf.R;
 import ch.epfl.sweng.swenggolf.database.Database;
 import ch.epfl.sweng.swenggolf.database.ValueListener;
 
@@ -19,5 +24,11 @@ public class ListOwnOfferActivity extends ListOfferActivity {
             }
         }, categories);
     }
-}
 
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
+        View view = super.onCreateView(inflater, container, savedInstance);
+        setToolbar(R.drawable.ic_menu_black_24dp, R.string.my_offers);
+        return view;
+    }
+}
