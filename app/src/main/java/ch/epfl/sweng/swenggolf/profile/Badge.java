c7112cdc75e082478a5f299468723081fb466641
dingepfl <markus.ding@epfl.ch>
2018-11-17 17:57:20 +0100

    Add javadoc for Badge class

--- app/src/main/java/ch/epfl/sweng/swenggolf/profile/Badge.java
+++ app/src/main/java/ch/epfl/sweng/swenggolf/profile/Badge.java
@@ -4,6 +4,11 @@ import ch.epfl.sweng.swenggolf.R;
 
 public final class Badge {
 
+    /**
+     * Returns the Drawable element of the badge corresponding to the points of a user
+     * @param points The amount of points of a user
+     * @return The Drawable of the badge
+     */
     public static int getDrawable(int points){
         int level = computeLevel(points);
         switch (level){
@@ -26,7 +31,13 @@ public final class Badge {
 
     }
 
+    /**
+     * Computes the Level corresponding to the amount of points
+     * @param points the amount of points of the user
+     * @return the level of the corresponding Badge
+     */
     private static int computeLevel(int points){
         return points/6+1;
+        // TODO: implement a MAX_LEVEL constant that can not be exceeded
     }
 }
