cdddc37073b141480c032601232b6e0052e511c2
dingepfl <markus.ding@epfl.ch>
2018-12-05 02:04:28 +0100

    Refactor code

--- app/src/androidTest/java/ch/epfl/sweng/swenggolf/OfflineTests.java
+++ app/src/androidTest/java/ch/epfl/sweng/swenggolf/OfflineTests.java
@@ -2,7 +2,6 @@ package ch.epfl.sweng.swenggolf;
 
 import android.content.Intent;
 import android.support.test.espresso.intent.Intents;
-import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -34,7 +33,7 @@ public class OfflineTests {
 
 
     @Test
-    public void signInActivityCheckDialog(){
+    public void signInActivityCheckDialog() {
         Config.quitTest();
         Intents.init();
         signInRule.launchActivity(new Intent());
@@ -43,20 +42,19 @@ public class OfflineTests {
         onView(withId(R.id.sign_in_button)).perform(click());
         onView(withText(android.R.string.yes)).perform(click());
         intended(toPackage("com.android.settings"));
-        //pressBack();
         signInRule.finishActivity();
         Network.setFalseforTest(false);
         Intents.release();
     }
 
     @Test
-    public void listOfferCheckDialog(){
+    public void listOfferCheckDialog() {
+        Config.quitTest();
         Intents.init();
         Network.setFalseforTest(true);
         listOfferRule.launchActivity(new Intent());
         onView(withText(android.R.string.yes)).perform(click());
         intended(toPackage("com.android.settings"));
-        //pressBack();
         Network.setFalseforTest(false);
         listOfferRule.finishActivity();
         Intents.release();
