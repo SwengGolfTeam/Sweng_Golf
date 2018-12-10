6c8ded94ed5b71138ba79a332362ac57e4270e41
dingepfl <markus.ding@epfl.ch>
2018-12-10 19:46:07 +0100

    Add tests for Stats in offers

---
+++ app/src/androidTest/java/ch/epfl/sweng/swenggolf/ShowOfferStatsTest.java
@@ -0,0 +1,124 @@
+package ch.epfl.sweng.swenggolf;
+
+import android.content.Intent;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.v4.app.FragmentTransaction;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import ch.epfl.sweng.swenggolf.database.Database;
+import ch.epfl.sweng.swenggolf.database.DbError;
+import ch.epfl.sweng.swenggolf.database.FakeDatabase;
+import ch.epfl.sweng.swenggolf.database.FilledFakeDatabase;
+import ch.epfl.sweng.swenggolf.database.ValueListener;
+import ch.epfl.sweng.swenggolf.main.MainMenuActivity;
+import ch.epfl.sweng.swenggolf.offer.Offer;
+import ch.epfl.sweng.swenggolf.profile.User;
+import ch.epfl.sweng.swenggolf.statistics.OfferStats;
+import ch.epfl.sweng.swenggolf.tools.FragmentConverter;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static junit.framework.TestCase.fail;
+import static org.hamcrest.Matchers.not;
+
+
+@RunWith(AndroidJUnit4.class)
+public class ShowOfferStatsTest {
+    @Rule
+    public final IntentsTestRule<MainMenuActivity> mActivityRule =
+            new IntentsTestRule<>(MainMenuActivity.class, false, false);
+
+    private User user = FilledFakeDatabase.getUser(0);
+    private Offer offer;
+
+    /**
+     * Set up a fake database, a fake user and launch activity.
+     */
+    @Before
+    public void setUp() {
+        Database.setDebugDatabase(FakeDatabase.fakeDatabaseCreator());
+        Config.setUser(user);
+        mActivityRule.launchActivity(new Intent());
+    }
+
+    @Test
+    public void statsAreDisplayedOnOwnOffer() {
+        offer = FilledFakeDatabase.getOffer(0);
+        OfferStats.initializeNbViews(offer);
+        FragmentTransaction transaction = mActivityRule.getActivity()
+                .getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.centralFragment,
+                FragmentConverter.createShowOfferWithOffer(offer))
+                .commit();
+        onView(withId(R.id.show_offer_views)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void statsAreHiddenOnNotOwnOffer() {
+        offer = FilledFakeDatabase.getOffer(1);
+        OfferStats.initializeNbViews(offer);
+        FragmentTransaction transaction = mActivityRule.getActivity()
+                .getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.centralFragment,
+                FragmentConverter.createShowOfferWithOffer(offer))
+                .commit();
+        onView(withId(R.id.show_offer_views)).check(matches(not(isDisplayed())));
+    }
+
+    @Test
+    public void statsAreIncremented() {
+        offer = FilledFakeDatabase.getOffer(2);
+        OfferStats.initializeNbViews(offer);
+
+        //visit offer n°2 with user n°0
+        FragmentTransaction transaction = mActivityRule.getActivity()
+                .getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.centralFragment,
+                FragmentConverter.createShowOfferWithOffer(offer))
+                .commit();
+
+        //visit offer n°2 with user n°8 (author)
+        Config.setUser(FilledFakeDatabase.getUser(8));
+        transaction = mActivityRule.getActivity()
+                .getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.centralFragment,
+                FragmentConverter.createShowOfferWithOffer(offer))
+                .commit();
+
+        // test if displayed and incremented
+        onView(withId(R.id.show_offer_views)).check(matches(isDisplayed()));
+        ValueListener<Integer> listener = new ValueListener<Integer>() {
+            @Override
+            public void onDataChange(Integer nb) {
+                assert (nb == 1); // incremented once
+            }
+
+            @Override
+            public void onCancelled(DbError error) {
+                fail();
+            }
+        };
+
+        OfferStats.getNbViews(listener, offer);
+    }
+
+    @Test
+    public void statsAreDefaultWhenDbNotWorking() {
+        Database.setDebugDatabase(new FakeDatabase(false));
+        offer = FilledFakeDatabase.getOffer(0);
+
+        FragmentTransaction transaction = mActivityRule.getActivity()
+                .getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.centralFragment,
+                FragmentConverter.createShowOfferWithOffer(offer))
+                .commit();
+        onView(withId(R.id.show_offer_views)).check(matches(isDisplayed()));
+    }
+}
